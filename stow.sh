0e11a20813c3283f98d28272cd0350ce0346613d
Matt Conde <matt.conde@gmail.com>
2021-07-22 12:12:20 +0100

    add DS_Store file clean up to scripts

--- stow.sh
+++ stow.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+find . -name ".DS_Store" -delete
+
 for d in */ ; do
   echo "stowing $d"
   stow --restow $d
--- unstow.sh
+++ unstow.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+find . -name ".DS_Store" -delete
+
 for d in */ ; do
   echo "unstowing $d"
   stow -D $d
