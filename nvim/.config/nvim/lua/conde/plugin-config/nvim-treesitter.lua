b7cec2757e21f8d0e76f223dc2fbf9f789a4de74
Matt Conde <matt.conde@gmail.com>
2021-10-30 18:13:23 +0100

    use typescript parser to highlight flowtype files

--- nvim/.config/nvim/lua/conde/plugin-config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/conde/plugin-config/nvim-treesitter.lua
@@ -1,12 +1,18 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- or a list of language parsers to install
-  ignore_install = {}, -- list of parsers to ignore installing
+  ensure_installed = "maintained",
+  ignore_install = {},
   highlight = {
     enable = true,
-    disable = {}, -- list of language that will be disabled
+    use_languagetree = true,
+    disable = {},
   },
   indent = {
     enable = true,
   },
 }
+
+local parsers = require "nvim-treesitter.parsers"
+local parser_config = parsers.get_parser_configs()
+
+parser_config.typescript.used_by = "flowtype"
 
