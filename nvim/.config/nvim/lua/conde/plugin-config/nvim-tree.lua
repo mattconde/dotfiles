af24a19e52f99cfa865f6d8664b2e43d4a44355b
Matt Conde <matt.conde@gmail.com>
2021-12-20 17:05:31 +0000

    add key mappings to nvim tree config

--- nvim/.config/nvim/lua/conde/plugin-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/conde/plugin-config/nvim-tree.lua
@@ -1,9 +1,17 @@
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
 require'nvim-tree'.setup {
   auto_close = true,
   view = {
     auto_resize = true,
     side = 'right',
-    width = 50
+    width = 50,
+    mappings = {
+      list = {
+        { key = "h", cb = tree_cb("close_node") },
+        { key = "l", cb = tree_cb("edit") },
+      }
+    }
   }
 }
 
