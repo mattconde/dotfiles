e7820d6ef71c5a31d4f8d777e00e04d3aa552834
Matt Conde <matt.conde@gmail.com>
2022-03-15 10:24:27 +0000

    add mapping to search currently selected files directory

--- nvim/.config/nvim/lua/conde/plugin-config/telescope.lua
+++ nvim/.config/nvim/lua/conde/plugin-config/telescope.lua
@@ -6,6 +6,32 @@ local sorters = require('telescope.sorters')
 local finders = require('telescope.finders')
 local utils = require('telescope.utils')
 
+function getPath(str)
+  if str == nil then
+    return str
+  end
+  local path = str:match("(.*[/\\])")
+
+  return path
+end
+
+-- grep directory of selected item
+local search_directory_of_selection = function(prompt_bufnr)
+  local content = action_state.get_selected_entry(prompt_bufnr)
+  print('content1: ' .. content[1])
+  local path = getPath(content[1])
+  if path == nil then
+    print('should search in cwd: ' .. vim.fn.getcwd())
+    path = vim.fn.getcwd()
+  end
+  print('path: ' .. path)
+  actions._close(prompt_bufnr, true)
+  require('telescope.builtin').live_grep({
+    prompt_title = 'Find in - ' .. path,
+    cwd = path,
+  })
+end
+
 require('telescope').setup {
   defaults = {
     vimgrep_arguments = {
@@ -23,12 +49,14 @@ require('telescope').setup {
       n = {
         ['<esc>'] = actions.close,
         ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-g>'] = search_directory_of_selection
       },
       i = {
         ['<esc>'] = actions.close,
         ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-g>'] = search_directory_of_selection
       },
     },
     file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
@@ -39,7 +67,7 @@ require('telescope').setup {
 
 local M = {}
 
--- live grep within current files directory
+-- grep within current files directory
 M.search_directories = function()
   local path = vim.fn.expand('%:h')
   local limitedPath = string.sub(path, -80)
