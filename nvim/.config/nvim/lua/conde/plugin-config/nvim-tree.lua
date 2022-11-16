1eff92f0e0f9419df3476108d0328c10c1dbce0b
Matt Conde <matt.conde@gmail.com>
2022-11-16 22:44:20 +0000

    remove settings in nvim tree that are now default in the newer version

--- nvim/.config/nvim/lua/conde/plugin-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/conde/plugin-config/nvim-tree.lua
@@ -1,9 +1,7 @@
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
 require'nvim-tree'.setup {
-  auto_close = true,
   view = {
-    auto_resize = true,
     side = 'right',
     width = 50,
     mappings = {
