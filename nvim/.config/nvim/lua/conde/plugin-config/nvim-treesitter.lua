9e656d3d28cdd001dec05614a0aa6083c0342d94
Matt Conde <matt.conde@gmail.com>
2022-11-16 22:38:36 +0000

    use treesitter auto install

--- nvim/.config/nvim/lua/conde/plugin-config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/conde/plugin-config/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all",
-  ignore_install = {},
+  auto_install = true,
   highlight = {
     enable = true,
     use_languagetree = true,
